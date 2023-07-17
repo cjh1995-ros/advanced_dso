537addfd6e3e08e8099118a317f7f718f4e4f1b9
cjh1995-ros <dkwnsgur12@gmail.com>
2023-07-17 14:09:37 +0900

    update code

---
+++ module/Image/Point.cpp
@@ -0,0 +1,17 @@
+#include "Image/Point.hpp"
+#include "Image/Pixel.hpp"
+
+namespace adso
+{
+
+void DepthPoint::SetIdepthInfo(double idepth, double info) noexcept
+{
+    
+}
+
+
+
+
+
+
+} // end of namespace adso
